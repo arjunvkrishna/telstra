ba1d7793d606d3db44a0a0bd074bdbd25abe6018
root <root@ip-10-61-69-197.ap-southeast-2.compute.internal>
2023-07-19 12:28:43 +0000

    yml update

---
+++ create_config.sh
@@ -0,0 +1,27 @@
+../sift-manage/sift setConfig patternmanager so-opolo patternmanager-cfg.properties
+
+../sift-manage/sift setConfig ocshil-fulfilmenthandler so-opolo ocshil-fulfilmenthandler-cfg.properties
+
+../sift-manage/sift setConfig mpsdiscounthandler so-opolo mpsdiscounthandler-cfg.properties
+
+../sift-manage/sift setConfig mpscloudhandler so-opolo mpscloudhandler-cfg.properties
+
+../sift-manage/sift setConfig fulfilmaster so-opolo fulfilmaster-cfg.properties
+
+../sift-manage/sift setConfig bu-router so-opolo bu-router-cfg.properties
+
+../sift-manage/sift setConfig smshandler so-opolo smshandler-cfg.properties
+
+../sift-manage/sift setConfig siftSfmcde so-opolo siftsfmcde-cfg.properties
+
+../sift-manage/sift setConfig siftconfigdump so-opolo siftconfigdump-cfg.properties
+
+../sift-manage/sift setConfig recordkeeper so-opolo recordkeeper-cfg.properties
+
+../sift-manage/sift setConfig interactionhandler-boost-bu so-opolo interactionhandler-boost_bu-cfg.properties
+
+../sift-manage/sift setConfig interactionhandler-cnsb-bu so-opolo interactionhandler-cnsb_bu-cfg.properties
+
+../sift-manage/sift setConfig smshandler-boost-bu so-opolo smshandler-boost-bu-cfg.properties
+
+../sift-manage/sift setConfig bu-router-interaction so-opolo bu-router-interaction-cfg.properties
